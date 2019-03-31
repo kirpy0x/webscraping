698b36842fe0166ab9c8461ebb5f8517112e6d39
kirpy0x <kkrrp1@gmail.com>
2019-03-31 02:41:42 -0600

    added feature to export results to csv file

--- Scrapegoogle.py
+++ Scrapegoogle.py
@@ -3,15 +3,16 @@ import requests
 import csv
 
 search_query = input("What would you like to search? ")
+filename = input("What would you like to name the exported csv file? ")
 source = requests.get(f"https://www.google.com/search?safe=off&source=hp&ei=SaqfXL2CGoGUtQXGiqwo&q={search_query}&btnK=Google+Search&oq={search_query}&gs_l=psy-ab.3...1396.2099..2179...0.0..0.100.643.5j2......0....1..gws-wiz.....0..35i39j0j0i131j0i10.U7HM6InUXJA").text
 
 soup = BeautifulSoup(source, 'lxml')
 
 
 #EXPORT RESULTS TO CSV FILE
-#csv_file = open('googleresults.csv', 'w')
-#csv_writer = csv(csv_file)
-#csv_writerow(['headline', 'url', 'date', 'summary'])
+csv_file = open(f'{filename}.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline', 'url', 'date', 'summary'])
 
  
 #print(soup.prettify)
@@ -21,20 +22,9 @@ for results in soup.find_all('div', class_='g', limit=10):
     headline = results.a.text
 
     weblink = results.find('cite').text
-#link = weblink['href']
+
     summary = results.find('span', class_="st").text
-#print(results.prettify())   
-    print(headline)
-    print(weblink)
-    print(summary)
+
+    csv_writer.writerow([headline, weblink, summary])
 
 
-
-
-#results = soup.find_all('h3 ', class_=("LC201b"))
-#print(results)
-#text = []
-#for result in results:
-#    text.append("********************************************************************")
-#    text.append(link.get_text())
-#print(text)
